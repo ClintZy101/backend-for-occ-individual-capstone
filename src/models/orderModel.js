0e6e27a460d62aef2c75774726dd50e78c6eb03b
clintzy <clielizek.joy@gmail.com>
2024-12-29 17:51:39 +0800

    added quantity property in cartItems orderSchema

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -12,6 +12,7 @@ const orderSchema = new mongoose.Schema(
         category: { type: [String], required: true },
         prod_info: { type: String, required: true },
         src: { type: String, required: true },
+        quantity: { type: Number, required: true },
       },
     ],
     shippingAddress: {
